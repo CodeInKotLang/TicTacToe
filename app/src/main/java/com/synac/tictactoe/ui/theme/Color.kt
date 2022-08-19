0d27d4a50cc932779d875f2bc0572353d4241059
Mohammad Arif <danielharold21@gmail.com>
2022-08-19 22:11:40 +0530

    Initial commit

---
+++ app/src/main/java/com/synac/tictactoe/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.synac.tictactoe.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Aqua = Color(0xFF04D4F0)
+val GreenishYellow = Color(0xFFA8C920)
+val GrayBackground = Color(0xFFf5f7f6)
+val BlueCustom = Color(0xFF3871df)
